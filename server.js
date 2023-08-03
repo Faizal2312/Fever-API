9d2c02a11a0f3c426fdd4db66a9bc802a5aac322
Faizal khan <faizalkhan.tk@gmail.com>
2023-08-02 22:02:42 -0700

    commit

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: "https://chipper-chimera-a1504c.netlify.app/",
+    origin: "https://chipper-chimera-a1504c.netlify.app",
     credentials: true,
   })
 );
